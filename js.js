f35cde9530dbddd6baad041e54620c6c74df0844
jeisonrosario66 <96961824+jeisonrosario66@users.noreply.github.com>
2023-02-04 00:36:54 -0300

    porfolio_up
    Agrego..
    -el sub menu colapta al precionar un enlaze

--- js.js
+++ js.js
@@ -1,9 +1,29 @@
 const elemento = document.getElementById("menu_burger");
+const elemento_sub = document.getElementById("sub_inicio");
+const elemento_sub2 = document.getElementById("sub_about");
+const elemento_sub3 = document.getElementById("sub_skills");
+const elemento_sub4 = document.getElementById("sub_portafolio");
+const elemento_sub5 = document.getElementById("sub_contacto");
+
+
 let interructor = false
+function sub() {
+    if (interructor === true) {
+        info_principal.classList.remove("if_prcp_dsplz");
+        info_principal.classList.add("if_prcp_cntr");
+
+        menu_lateral.classList.remove("mn_ltl_active");
+        menu_lateral.classList.add("mn_ltl_none");
+        
+        elemento.classList.remove("fa-bars-staggered")
+        elemento.classList.add("fa-bars")
+
+        interructor = false
+    }
+}
 function validaClick(){
     const menu_lateral = document.getElementById("menu_lateral");
     const info_principal = document.getElementById("info_principal");
-
     if (interructor === false) {
         info_principal.classList.remove("if_prcp_cntr");
         info_principal.classList.add("if_prcp_dsplz");
@@ -13,7 +33,6 @@ function validaClick(){
 
         elemento.classList.remove("fa-bars")
         elemento.classList.add("fa-bars-staggered")
-
 
         interructor = true
     }else {
@@ -31,4 +50,10 @@ function validaClick(){
 };
 
 elemento.addEventListener("click", validaClick);
+elemento_sub.addEventListener("click", sub);
+elemento_sub2.addEventListener("click", sub);
+elemento_sub3.addEventListener("click", sub);
+elemento_sub4.addEventListener("click", sub);
+elemento_sub5.addEventListener("click", sub);
+
 
